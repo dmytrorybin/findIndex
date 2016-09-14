b271c69ff6eb14375903a0429e46042e1fe0267c
Dmytro Rybin <dmytro_rybin@epam.com>
2016-09-14 17:46:38 +0300

    TDD method added

--- src/main/java/ua/epam/Method.java
+++ src/main/java/ua/epam/Method.java
@@ -4,26 +4,26 @@ package ua.epam;
  * Created by Dmytro_Rybin on 9/14/2016.
  */
 public class Method {
-//    private int count = 0;
-//
-//    private int isPresent(int[] arr, int value) {
-//        for (int i = 0; i < arr.length; i++) {
-//            if (value == arr[i]) {
-//                count++;
-//            }
-//        }
-//        return count;
-//    }
-//
-//    public int[] finder(int[] arr, int value) {
-//        isPresent(arr, value);
-//        int j = 0;
-//        int [] res = new int[count];
-//        for (int i = 0; i < arr.length; i++) {
-//            if (value == arr[i]) {
-//                res[j++] = i;
-//            }
-//        }
-//        return res;
-//    }
+    private int count = 0;
+
+    private int isPresent(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int[] finder(int[] arr, int value) {
+        isPresent(arr, value);
+        int j = 0;
+        int [] res = new int[count];
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                res[j++] = i;
+            }
+        }
+        return res;
+    }
 }
